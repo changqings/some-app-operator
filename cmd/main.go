a35d471282c852d2b1b10f5538d5695005949023
shenchangqing <shenqinpeng@126.com>
2024-07-02 20:18:14 +0800

    Add cache.Option, exclude not cahce kube-system ns

--- cmd/main.go
+++ cmd/main.go
@@ -26,10 +26,12 @@ import (
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 
 	istio_network_v1beta1 "istio.io/client-go/pkg/apis/networking/v1beta1"
+	"k8s.io/apimachinery/pkg/fields"
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/cache"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
@@ -73,6 +75,14 @@ func main() {
 	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
 
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
+		// Cache not include kube-system or other namespace
+		Cache: cache.Options{
+			DefaultFieldSelector: fields.ParseSelectorOrDie("metadata.name!=kube-system,metadata.name!=kube-node-lease"),
+			// DefaultNamespaces: map[string]cache.Config{
+			// 	"default": {},
+			// 	"some-ns": {},
+			// },
+		},
 		Scheme:                 scheme,
 		Metrics:                metricsserver.Options{BindAddress: metricsAddr},
 		HealthProbeBindAddress: probeAddr,
