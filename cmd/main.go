f5e349a328062cd27acbc88281efbc84c8949f5f
shenchangqing <shenqinpeng@126.com>
2024-07-02 21:08:10 +0800

    cache default only cr of you crd

--- cmd/main.go
+++ cmd/main.go
@@ -26,12 +26,10 @@ import (
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 
 	istio_network_v1beta1 "istio.io/client-go/pkg/apis/networking/v1beta1"
-	"k8s.io/apimachinery/pkg/fields"
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/cache"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
@@ -76,13 +74,13 @@ func main() {
 
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
 		// Cache not include kube-system or other namespace
-		Cache: cache.Options{
-			DefaultFieldSelector: fields.ParseSelectorOrDie("metadata.name!=kube-system,metadata.name!=kube-node-lease"),
-			// DefaultNamespaces: map[string]cache.Config{
-			// 	"default": {},
-			// 	"some-ns": {},
-			// },
-		},
+		// Cache: cache.Options{
+		// 	DefaultFieldSelector: fields.ParseSelectorOrDie("metadata.namespace!=kube-system,metadata.namespace!=kube-node-lease"),
+		// 	DefaultNamespaces: map[string]cache.Config{
+		// 		"default": {},
+		// 		"some-ns": {},
+		// 	},
+		// },
 		Scheme:                 scheme,
 		Metrics:                metricsserver.Options{BindAddress: metricsAddr},
 		HealthProbeBindAddress: probeAddr,
